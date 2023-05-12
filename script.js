e5702f666a316f25309d4a729ad4b0b89b852786
SashaTrincher <oofman@ukr.net>
2023-05-12 18:23:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let str = 'Hello World!';
+
+console.log(`The length of the string is ${str.length}`);
+console.log(`The string in all lowercase is ${str.toLowerCase()}`);
+console.log(`The string in all uppercase is ${str.toUpperCase()}`);
+console.log(`The index of the letter 'o' in the string is ${str.indexOf('o')}`);
+console.log(`Does the string include the word 'World'? ${str.includes('World')}`);
